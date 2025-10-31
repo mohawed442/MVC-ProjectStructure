dfc3119ea634e080c50cc26500ac15efc88ee4dd
mohamed zakaria <mohamed_zakaria@gmail.com>
2025-10-31 06:54:45 +0200

    login The login page has been created

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+import * as dotenv from 'dotenv'
+import path from 'path';
+
+dotenv.config({ path: path.resolve('.env') });
+
+console.log({ path: path.resolve('.env') });
+
+
+const dbConnect = ()=>{
+    mongoose.connect(process.env.MONGO_URI)
+    .then(()=>console.log("connect db"))
+    .catch((err)=>console.log(err , "error db"))
+}
+
+export default dbConnect
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,6 +2,7 @@ import { login} from "../controllers/auth.controller.js";
 import express from "express";
 const router = express.Router();
 
+router.post("/login", login);
 
 
 export default router;
