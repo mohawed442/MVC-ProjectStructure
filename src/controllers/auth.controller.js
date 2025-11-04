50770f35d4976e23c23f03fb2ea91c441ba1c2b3
mohamed zakaria <mohamed_zakaria@gmail.com>
2025-11-04 21:11:13 +0200

    add file validation

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,16 +1,25 @@
-const login = async (req, res, next) => {
+import bcrypt from "bcryptjs";
+import { emailEvent } from "../utils/email-event.js";
+import ApiError from "../utils/api-error.js";
+import ApiResponse from "../utils/api-response.js";
+import generateCode from "../utils/generate-code.js";
+import { generateOTPToken } from "../utils/jwt.js";
+import User from "../models/user.model.js";
+import OTP from "../models/OTP.model.js";
+
+export const login = async (req, res, next) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
 
   if (!user) {
-    return next(new ApiError("in-valid login Data", 400));
+    return next(new ApiError("in-valid login Data", 401));
   }
 
   const match = await bcrypt.compare(password, user.password);
 
   if (!match) {
-    return next(new ApiError("in-valid login Data", 400));
+    return next(new ApiError("in-valid login Data", 401));
   }
 
   const code = generateCode();
@@ -19,7 +28,7 @@ const login = async (req, res, next) => {
   emailEvent.emit("sendConfirmEmail", { email, code });
 
   if (!user.isVerified) {
-    return next(new ApiError("in-valid login Data", 400));
+    return next(new ApiError(" user is not verified check your email", 403));
   }
 
   const token = generateOTPToken(String(user._id));
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,8 @@
-
 import express from "express";
+import { loginSchema } from "../validators/auth.validator.js";
+import { login } from "../controllers/auth.controller.js";
+import validate from "../middlewares/validate.middleware.js";
+
 const router = express.Router();
 
 router.post("/login",validate(loginSchema, "body"), login);
